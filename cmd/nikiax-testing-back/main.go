304ece5006c0dd808e3a279241d905d4a2e02ec7
Alexey Mamontov <gakkerrs@gmail.com>
2025-03-09 18:58:10 +0700

    Revert "Добавил подключение к базе данных"
    This reverts commit b3a074477d41e8ac85221074ccae5d6a2f2a4363.

--- cmd/nikiax-testing-back/main.go
+++ cmd/nikiax-testing-back/main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"net/http"
-	"nikiax-testing-back/internal/database"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
-	_ "github.com/lib/pq"
 )
 
 func main() {
-
-	db := database.MustLoadDB()
-	defer db.Close()
-
-	_, err := db.Exec("INSERT INTO tests (name, alias, result, stage) VALUES ($1, $2, $3, $4)", "t5", "test t5", 0, 5)
-	if err != nil {
-		panic(err)
-	}
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
